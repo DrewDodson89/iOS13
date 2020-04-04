6bb7d6f615a8e66b974525f0246700f48788bc16
Drew Dodson <drew.dodson@protonmail.com>
2020-04-04 11:39:17 -0700

    Completed 'I am Rich' with SwiftUI

---
+++ Section 17/I am Rich/I am Rich/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  I am Rich
+//
+//  Created by Drew Dodson on 4/4/20.
+//  Copyright © 2020 Arcalla.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
