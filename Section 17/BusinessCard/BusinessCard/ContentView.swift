3391a5b0dc9821c27567f1e1cf3dcf5f18ec51df
Drew Dodson <drew.dodson@protonmail.com>
2020-04-05 08:36:45 -0700

    Completed 'I am Rich' with SwiftUI

---
+++ Section 17/BusinessCard/BusinessCard/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by Drew Dodson on 4/5/20.
+//  Copyright © 2020 Arcalla.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+a
