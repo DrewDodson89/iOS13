cf72ad52089e6833cdd7b215bc60ce013182d37a
Drew Dodson <drew.dodson@protonmail.com>
2020-04-29 07:32:49 -0700

    Complete 'In-app purchases'

---
+++ Section 20/InspoQuotes/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  InspoQuotes
+//
+//  Created by Drew Dodson on 4/28/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let cellID = "quoteCell"
+    static let productID = "com.arcalla.inspoquotes.premiumquotes"
+}
